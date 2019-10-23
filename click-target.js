7834312d4ec497a2f4500df953a4a8bcbaaa9b10
piyo2019 <hakobe2001@yahoo.co.jp>
2019-10-23 14:46:44 +0900

    同じようなコードを配列を用いてまとめました

--- click-target.js
+++ click-target.js
@@ -5,7 +5,18 @@
  *   2019/10/22: 変数宣言の修正（varをconstまたはletにした）
  *      ・ tn はletで宣言（ const で宣言すると動かなかった）
  *      ・ tn1~tn3は const で宣言（let でないとダメかと思ったが， 動いた
- *   2019/10/23：変数tnの宣言と代入の行を修正（まとめてconstで変数宣言）    
+ *   2019/10/23：変数tnの宣言と代入の行を修正（まとめてconstで変数宣言）
+ *  -----
+ *    201910/23:関数rotateTargetの合理化
+ *      ・配列を利用して同じようなものをまとめた ＆まとめるために配列化
+ *          rotateTarget1～3 を rotateTarget[1]~[3] 
+ *          fl1～3 を fl[1]~[3]
+ *          timer1～3 を timer[1]~[3]
+ *          target1～3 を target[1]~[3]
+ *          tn1～3 を targetNumber[1]~[3] ※変数名注意
+ *       ・数字を出すスピードを，変数 windowNumberSpeed として定義
+ *             クリックしたときに出てた数字と取得するwindowNumberのズレがごまかせるよう速め（50）に設定したが，速すぎる
+ *        ・変数名の変更：tn　→　tnBase 
  */
 'use strict';
 const resultArea =  document.getElementById('result-area');
@@ -14,28 +25,38 @@ const retry =  document.getElementById('retry');
 let nokoriKai = 5;
 let shootNumber = null;
 let shootNumberList = '';
-let fl1 = 1; //target1が表なら1，裏なら0
-let fl2 = 1; //target2が表なら1，裏なら0
-let fl3 = 1; //target3が表なら1，裏なら0
+
+let fl =[ ,1,1,1]; // fl[i]（i=1,2,3 ）は target[i]が表なら1，裏なら0）※f[0]は定義しない
+
+let target =[];
+target[1] = document.getElementById('target1');
+target[2] = document.getElementById('target2');
+target[3] = document.getElementById('target3');
+
+let rotateTarget = []; //関数rotateTarget[i](i=1～3)を入れる配列
+let timer = [];// rotateTarget[i](i=1～3)で使うtimer[i]の配列
 
 const kaitensokudo = 40; // 的が裏返る速さはここで調整（setInterval の間隔）
+const windowNumberSpeed = 50; // 窓の数字を出すスピード
 
 // 的の数字  --- (new Date() の1桁目～3桁目が的の番号になる)
-const tn = new Date().getTime(); 
-const tn1 = tn % 10; // target1の数字
-const tn2 = Math.floor(tn / 10) % 10; // target2の数字
-const tn3 = Math.floor(tn / 100) % 10; // target3の数字
-document.getElementById('target1').innerText = tn1;
-document.getElementById('target2').innerText = tn2;
-document.getElementById('target3').innerText = tn3;
-document.getElementById('target-number-list').innerText = '的の数字：' + tn1 + ' ' + tn2 + ' ' + tn3;
+const tnBase = new Date().getTime(); 
+const targetNumber =[ ,//targetNumber[0]は定義しない
+    tnBase % 10, // targetNumber[1] :target1の数字
+    Math.floor(tnBase / 10) % 10, // targetNumber[2]: target2の数字
+    Math.floor(tnBase / 100) % 10 // targetNumber[3]: target3の数字
+];
+target[1].innerText = targetNumber[1];
+target[2].innerText = targetNumber[2];
+target[3].innerText = targetNumber[3];
+document.getElementById('target-number-list').innerText = '的の数字：' + targetNumber[1] + ' ' + targetNumber[2] + ' ' + targetNumber[3];
 
 //  スタートボタンを押したらカウンタースタート
 let windowNumber = 0;
 let windowTimer = null;
 document.getElementById('start-button').onclick = function () {
     if (nokoriKai !== 0) {
-        windowTimer = setInterval(windowNumberShow, 50); //　数字を出すスピードはここで調節---クリックしたときに出てた数字と取得するwindowNumberのズレがごまかせるよう速めに設定する！
+        windowTimer = setInterval(windowNumberShow, windowNumberSpeed); //　数字を出すスピードはwindowNumberSpeedで調節
     }
 }
 
@@ -60,7 +81,7 @@ document.getElementById('tn-button').onclick = function () {
 
 //　 窓に0～9の数字を出す & シュートしたときの処理
 function windowNumberShow() {
-    if ((nokoriKai === 0) || (fl1 + fl2 + fl3 === 0)) {
+    if ((nokoriKai === 0) || (fl[1] + fl[2] + fl[3] === 0)) {
         clearInterval(windowTimer);
         setTimeout(result, 1000); // 1秒まって結果を表示
     }
@@ -73,107 +94,55 @@ function windowNumberShow() {
             shootNumberList = shootNumberList + shootNumber;
             nokoriKai = nokoriKai - 1;
             document.getElementById('shoot-number-list').innerText = 'シュートした数字：' + shootNumberList;
-            if (shootNumber === tn1 && fl1 === 1) {
-                rotateTarget1();
-                fl1 = 0;
+            for(let k=1; k <=3;k++){
+                if (shootNumber === targetNumber[k] && fl[k] === 1) {
+                    rotateTarget[k]();
+                    fl[k] = 0;
+                }
             }
-            if (shootNumber === tn2 && fl2 === 1) {
-                rotateTarget2();
-                fl2 = 0;
-            }
-            if (shootNumber === tn3 && fl3 === 1) {
-                rotateTarget3();
-                fl3 = 0;
-            }
-        }
+         }
     };
     windowNumber = (windowNumber + 1) % 10;
 }
 
-// 的1を回転
-function rotateTarget1() {
-
-    fl1 == 0;
-    let timer1 = null; // タイマーのID
-    let kaitenkaku = 0;
-    function rotate() {
-        kaitenkaku = kaitenkaku + 6;
-        //半周回ったらストップ
-        if (kaitenkaku > 180 && timer1 !== null) {
-            clearInterval(timer1); // タイマー停止
+// 的を回転させる関数
+for(let i = 1; i <=3; i++){
+    // 的i(i=1,2,3)を回転
+    rotateTarget[i] = function () {
+        fl[i] == 0;
+        timer[i]= null; // タイマーのID
+        let kaitenkaku = 0;
+        function rotate() {
+            kaitenkaku = kaitenkaku + 6;
+            //半周回ったらストップ
+            if (kaitenkaku > 180 && timer[i] !== null) {
+                clearInterval(timer[i]); // タイマー停止
+            }
+            //回転角によって状態を変える
+            if (0 <= kaitenkaku && kaitenkaku < 90) {
+                target[i].classList.remove('cannotSee');
+                target[i].classList.add('canSee');
+            } else {
+                target[i].classList.remove('canSee');
+                target[i].classList.add('cannotSee');
+            }
+            target[i].style.transform = 'rotateX(' + kaitenkaku + 'deg)';
         }
-        //回転角によって状態を変える
-        if (0 <= kaitenkaku && kaitenkaku < 90) {
-            target1.classList.remove('cannotSee');
-            target1.classList.add('canSee');
-        } else {
-            target1.classList.remove('canSee');
-            target1.classList.add('cannotSee');
-        }
-        target1.style.transform = 'rotateX(' + kaitenkaku + 'deg)';
-    }
-    timer1 = setInterval(rotate, kaitensokudo);
-}
-
-// 的2を回転
-function rotateTarget2() {
-    fl2 == 0;
-    let timer2 = null; // タイマーのID
-    let kaitenkaku = 0;
-    function rotate() {
-        kaitenkaku = kaitenkaku + 6;
-        //半周回ったらストップ
-        if (kaitenkaku > 180 && timer2 !== null) {
-            clearInterval(timer2); // タイマー停止
-        }
-        //回転角によって状態を変える
-        if (0 <= kaitenkaku && kaitenkaku < 90) {
-            target2.classList.remove('cannotSee');
-            target2.classList.add('canSee');
-        } else {
-            target2.classList.remove('canSee');
-            target2.classList.add('cannotSee');
-        }
-        target2.style.transform = 'rotateX(' + kaitenkaku + 'deg)';
-    }
-    timer2 = setInterval(rotate, kaitensokudo);
-}
-
-// 的3を回転
-function rotateTarget3() {
-    fl3 == 0;
-    let timer3 = null; // タイマーのID
-    let kaitenkaku = 0;
-    function rotate() {
-        kaitenkaku = kaitenkaku + 6;
-        //半周回ったらストップ
-        if (kaitenkaku > 180 && timer3 !== null) {
-            clearInterval(timer3); // タイマー停止
-        }
-        //回転角によって状態を変える
-        if (0 <= kaitenkaku && kaitenkaku < 90) {
-            target3.classList.remove('cannotSee');
-            target3.classList.add('canSee');
-        } else {
-            target3.classList.remove('canSee');
-            target3.classList.add('cannotSee');
-        }
-        target3.style.transform = 'rotateX(' + kaitenkaku + 'deg)';
-    }
-    timer3 = setInterval(rotate, kaitensokudo);
+        timer[i] = setInterval(rotate, kaitensokudo);
+    }    
 }
 
 // ゲームの結果を表示する関数
 function result() {
     resultArea.classList.remove('before');
     resultArea.classList.add('after');
-    if (fl1 + fl2 + fl3 === 0) {
+    if (fl[1] + fl[2] + fl[3] === 0) {
         document.getElementById('result-area').innerText = 'パーフェクト！ すごい！';
-    } else if (fl1 + fl2 + fl3 === 1) {
+    } else if (fl[1] + fl[2] + fl[3] === 1) {
         document.getElementById('result-area').innerText = '2つ成功！ やったね！';
-    } else if (fl1 + fl2 + fl3 === 2) {
+    } else if (fl[1] + fl[2] + fl[3] === 2) {
         document.getElementById('result-area').innerText = '1つ成功！ まずまず。';
-    } else if (fl1 + fl2 + fl3 === 3) {
+    } else if (fl[1] + fl[2] + fl[3] === 3) {
         document.getElementById('result-area').innerText = '残念！';
     }
     retry.classList.remove('before'); // 「もう1回する」ボタンを見せる
